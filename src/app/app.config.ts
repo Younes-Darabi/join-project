548fc966728e333e0f2fb7abc4cd764ff647841a
Younes Darabi <younes.darabi@outlook.com>
2025-11-12 11:28:07 +0100

    add basic/navbar/header

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "join-f1979", appId: "1:736216917449:web:f234c0dc422382891cf418", storageBucket: "join-f1979.firebasestorage.app", apiKey: "AIzaSyCewJLxvkjIQ_cxyETz84VI1MlGgJKA2-w", authDomain: "join-f1979.firebaseapp.com", messagingSenderId: "736216917449", measurementId: "G-RHNTBLV4WN" })), provideFirestore(() => getFirestore())
   ]
 };
---
+++ src/app/components/page-one/page-one.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-one',
+  imports: [],
+  templateUrl: './page-one.html',
+  styleUrl: './page-one.scss',
+})
+export class PageOne {
+
+}
