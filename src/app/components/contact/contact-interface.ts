da16262556657c0da56a58960bb9877d52fee173
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-11-13 12:21:28 +0100

    contact icons added and contact-details implementation started

---
+++ src/app/components/contact/contact-interface.ts
@@ -0,0 +1,9 @@
+export interface ContactInterface {
+    id?: string;
+    email: string;
+    color: string;  
+    fullname?: string;
+    firstname: string;
+    lastname: string;
+    phone: string;    
+}
