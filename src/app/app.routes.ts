11d96693963a950ec00bdd38bdbe88c993ff5076
Kevin Hase <hasekevin21@googlemail.com>
2025-11-17 01:30:26 +0100

    add some changes at contact_list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Contact } from './components/contact/contact';
+import { Add } from './components/contact/add/add';
 
 export const routes: Routes = [
-    { path: '', component: Contact}
+    { path: '', component: Contact},
+    {path: 'add', component: Add},
 ];
