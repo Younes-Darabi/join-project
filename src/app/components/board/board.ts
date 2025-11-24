376174dfd92143496e777d38546338e01f3cb27d
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-11-24 16:38:41 +0100

    started wit drag and drop

--- src/app/components/board/board.ts
+++ src/app/components/board/board.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject, PLATFORM_ID } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+
+import { Router } from '@angular/router';
+
+import { ActivatedRoute } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
