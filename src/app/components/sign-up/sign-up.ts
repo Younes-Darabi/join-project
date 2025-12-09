b9c39859f8bc8a819c77b900c7d1feaeecdc793c
Younes Darabi <younes.darabi@outlook.com>
2025-12-09 22:59:48 +0100

    errors fixed

--- src/app/components/sign-up/sign-up.ts
+++ src/app/components/sign-up/sign-up.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface Signup {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.scss',
 })
 export class SignUp {
-  privacy : boolean = false;
+  privacy: boolean = false;
+  checkMatchPassword: boolean = false;
+  signUp: Signup = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  }
+
+  onSubmit(signupForm: NgForm) {
+    this.checkMatchPassword = this.checkMatchPasswords();
+    if (!signupForm.invalid && !this.checkMatchPassword && this.privacy) {
+      console.log('Hi');
+      
+    }
+  }
+
+  checkMatchPasswords() {
+    if (this.signUp.password.length < 6 || this.signUp.password !== this.signUp.confirmPassword) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 }
