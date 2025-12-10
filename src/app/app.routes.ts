afaccdab30e09a1e7dc228253964dc9cf6a3e053
Kevin Hase <hasekevin21@googlemail.com>
2025-12-11 00:00:12 +0100

    start with privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Contact } from './components/contact/contact';
 import { Board } from './components/board/board';
 import { AddTask } from './components/add-task/add-task';
 import { Summary } from './components/summary/summary';
+import { PrivacyPolicy } from './components/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
     { path: 'Summary', component: Summary},
     { path: 'AddTask', component: AddTask},
     { path: 'Board', component: Board},
-    { path: 'Contact', component: Contact}
+    { path: 'Contact', component: Contact},
+    { path: 'PrivacyPolicy', component: PrivacyPolicy}
 ];
