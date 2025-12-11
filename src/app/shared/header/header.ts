e151cd4b40ee4e0b687b6e11920a07e51c7f59eb
Younes Darabi <younes.darabi@outlook.com>
2025-12-11 23:57:02 +0100

    menu header

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,14 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgStyle, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  menu : boolean = false ;
 
 }
