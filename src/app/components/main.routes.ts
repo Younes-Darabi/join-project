42300e0f0781b4381e1b3c77c6cccad04dca6b2b
Younes Darabi <younes.darabi@outlook.com>
2025-12-15 00:01:46 +0100

    add navbar routes

--- src/app/components/main.routes.ts
+++ src/app/components/main.routes.ts
@@ -4,6 +4,8 @@ import { Summary } from './summary/summary';
 import { AddTask } from './add-task/add-task';
 import { Board } from './board/board';
 import { Contact } from './contact/contact';
+import { PrivacyPolicy } from './privacy-policy/privacy-policy';
+import { LegalNotice } from './legal-notice/legal-notice';
 
 export const MainRoutes: Routes = [
   {
@@ -15,6 +17,8 @@ export const MainRoutes: Routes = [
       { path: 'add-task', component: AddTask },
       { path: 'board', component: Board },
       { path: 'contact', component: Contact },
+      { path: 'privacy-policy', component: PrivacyPolicy },
+      { path: 'legal-notice', component: LegalNotice },
     ]
   }
 ];
