fa770f0c3354b9435f80c1d671f42ec9fd0efb68
Younes Darabi <younes.darabi@outlook.com>
2025-12-14 22:05:04 +0100

    header update log in short name

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,7 +1,8 @@
 import { NgStyle } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { AuthService } from '../../services/auth/auth-service';
+import { getAuth, onAuthStateChanged } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-header',
@@ -9,17 +10,24 @@ import { AuthService } from '../../services/auth/auth-service';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
+export class Header implements OnInit {
   authService = inject(AuthService);
   menu: boolean = false;
   userShort: string = 'GA';
 
   ngOnInit() {
-    this.loadUserData();
+    const auth = getAuth();
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        await this.loadUserData();
+      } else {
+        this.userShort = 'GA';
+      }
+    });
   }
 
-  logOut(){
-    this.authService.logout()
+  logOut() {
+    this.authService.logout();
     this.userShort = 'GA';
   }
 
@@ -27,6 +35,8 @@ export class Header {
     const fullName = await this.authService.getCurrentFullName();
     if (fullName && this.authService.isLogin) {
       this.userShort = fullName.firstName[0] + fullName.lastName[0];
+    } else {
+      this.userShort = 'GA';
     }
   }
 }
