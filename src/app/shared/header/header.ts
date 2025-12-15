b48021a3d1afec79853f948e0fc19ca253af00f4
Kevin Hase <hasekevin21@googlemail.com>
2025-12-15 09:36:07 +0100

    fix header

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  private ignoreDocumentClick = false;
 
+  constructor(private elementRef: ElementRef) {}
+
+  toggleMenu() {
+    let menu = document.querySelector('.burgermenu') as HTMLElement;
+    this.ignoreDocumentClick = true;
+    setTimeout(() => this.ignoreDocumentClick = false, 0);
+    if (window.innerWidth < 1000) {
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (this.ignoreDocumentClick) return;
+    let target = event.target as Node;
+    let menu = document.querySelector('.burgermenu') as HTMLElement | null;
+    if (!menu) return;
+    if (!menu.contains(target) && menu.style.display === 'block') {
+      menu.style.display = 'none';
+    }
+  }
 }
